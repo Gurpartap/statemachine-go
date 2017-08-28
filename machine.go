ff5feabd6268d0e7d951a713e3a97d3c4f024842
Gurpartap Singh <contact@gurpartap.com>
2017-08-28 22:53:29 +0530

    add machine struct{} implementing Machine

--- machine.go
+++ machine.go
@@ -9,3 +9,36 @@ type Machine interface {
 	Fire(event string) error
 }
 
+// BuildNewMachine creates a zero-valued instance of machine, and builds it
+// using the passed machineBuilderFn arg.
+func BuildNewMachine(machineBuilderFn func(machineBuilder MachineBuilder)) Machine {
+	machine := NewMachine()
+	machine.Build(machineBuilderFn)
+	return machine
+}
+
+// NewMachine returns a zero-valued instance of machine, which implements
+// Machine.
+func NewMachine() Machine {
+	return &machine{}
+}
+
+type machine struct{}
+
+func (m *machine) Build(machineBuilderFn func(machineBuilder MachineBuilder)) {
+	machineBuilder := NewMachineBuilder()
+	machineBuilderFn(machineBuilder)
+	machineBuilder.Build(m)
+}
+
+func (m *machine) GetState() string {
+	panic("not implemented")
+}
+
+func (m *machine) IsState(state string) bool {
+	panic("not implemented")
+}
+
+func (m *machine) Fire(event string) error {
+	panic("not implemented")
+}
